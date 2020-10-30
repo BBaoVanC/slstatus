9a3887bb636a37c299fa553a94a5f01c53ada2a8
BBaoVanC <github@bbaovanc.com>
2020-10-29 19:53:57 -0500

    Add basic information

--- config.h
+++ config.h
@@ -63,5 +63,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, "| CPU: %s% |", NULL },
+	{ disk_free, " DISK: %s |", "/" },
+	{ ram_used, " RAM: %s", NULL },
+	{ ram_total, "/%s |", NULL },
+	{ keymap, " KBD: %s |", NULL },
+	{ datetime, " %s",           "%F %r" },
 };
